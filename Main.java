7e8efec9a5744482dfbe31c0a497d8022b0b6cbb
mcgml <mcgml@cardiff.ac.uk>
2015-07-02 17:18:20 +0100

    updated to allow no transcripts

--- Main.java
+++ Main.java
@@ -16,43 +16,45 @@ public class Main {
 
     public static void main(String[] args) {
 
-        if (args.length != 3) {
-            System.err.println("Usage: <BedFile> <VepFile> <Transcripts>");
+        if (args.length != 2 && args.length != 3) {
+            System.err.println("Usage: <BedFile> <VepFile> [Transcripts]");
             System.exit(1);
         }
 
-        ArrayList<String> nameTemp = new ArrayList();
-        File bedFilePath = new File(args[0]);
-        File vepFilePath = new File(args[1]);
-        File transcriptFilePath = new File(args[2]);
-
-        String[] filename = bedFilePath.getName().split("\\.");
-
-        //read preferred transcripts
+        ArrayList<String> nameTemp = new ArrayList<>();
+        String[] filename = args[0].split("\\.");
         HashSet<String> preferredTranscripts = new HashSet<>();
-        try (BufferedReader transcriptReader = new BufferedReader(new FileReader(transcriptFilePath))) {
 
-            String line;
+        if (args.length == 3){
 
-            while ((line = transcriptReader.readLine()) != null) {
-                if (!line.equals("")){
-                    String[] fields = line.split("\t");
-                    preferredTranscripts.add(fields[1]);
+            try (BufferedReader transcriptReader = new BufferedReader(new FileReader(new File(args[2])))) {
+
+                String line;
+
+                while ((line = transcriptReader.readLine()) != null) {
+                    if (!line.equals("")){
+
+                        String[] fields = line.split("\t");
+                        preferredTranscripts.add(fields[1]);
+
+                    }
                 }
+
+                transcriptReader.close();
+            } catch (IOException e) {
+                log.log(Level.SEVERE, "Could not read transcript file: " + e.getMessage());
             }
 
-        } catch (IOException e) {
-            log.log(Level.SEVERE, e.getMessage());
         }
 
         //read annotation file
-        VEPAnnotationFile annotationFile = new VEPAnnotationFile(vepFilePath);
+        VEPAnnotationFile annotationFile = new VEPAnnotationFile(new File(args[1]));
         annotationFile.parseVEP();
 
         //read bed file for annotation
         try (PrintWriter printWriter = new PrintWriter(new File(filename[0] + "_Annotated.bed"))){
 
-            try (AbstractFeatureReader reader = AbstractFeatureReader.getFeatureReader(bedFilePath.toString(), new BEDCodec(BEDCodec.StartOffset.ZERO), false)){
+            try (AbstractFeatureReader reader = AbstractFeatureReader.getFeatureReader(args[0], new BEDCodec(BEDCodec.StartOffset.ZERO), false)){
                 Iterable<BEDFeature> iter = reader.iterator();
 
                 //loop over bed records
@@ -62,29 +64,53 @@ public class Main {
                     printWriter.write(feature.getStart() + "\t");
                     printWriter.write(feature.getEnd() + "\t");
 
-                    if (!feature.getName().equals("")) {
-                        printWriter.write(feature.getName());
-                        printWriter.write(";");
-                    }
-
                     //loop over annotations for this region & print HGVS
                     if (annotationFile.getTranscriptLevelRecords().containsKey(feature.getContig() + ":" + feature.getStart() + "-" + feature.getEnd())){ //check if annotations exist
 
                         //loop over annotations
                         for (VEPTranscriptAnnotation ann : annotationFile.getTranscriptLevelRecords().get(feature.getContig() + ":" + feature.getStart() + "-" + feature.getEnd()))
                         {
-                            if (preferredTranscripts.size() != 0){ //if transcripts are provided; filter
+                            if (preferredTranscripts.size() > 0){ //if transcripts are provided; filter
 
                                 if (preferredTranscripts.contains(ann.getFeature())){
+
+                                    String exonOrIntronNumber = "";
+
+                                    if (!ann.getExonNo().equals("")){
+                                        exonOrIntronNumber = "e" + ann.getExonNo();
+                                    } else if (!ann.getIntronNo().equals("")){
+                                        exonOrIntronNumber = "i" + ann.getIntronNo();
+                                    }
+
                                     String[] fields = ann.getHgvsCoding().split("del");
-                                    nameTemp.add(ann.getFeature() + ":" + ann.getSymbol() + ":" + fields[0]);
+                                    nameTemp.add(ann.getFeature() + ":" + ann.getSymbol() + ":" + exonOrIntronNumber + ":" + fields[0]);
                                 }
 
                             } else {
+
+                                String exonOrIntronNumber = "";
+
+                                if (!ann.getExonNo().equals("")){
+                                    exonOrIntronNumber = "e" + ann.getExonNo();
+                                } else if (!ann.getIntronNo().equals("")){
+                                    exonOrIntronNumber = "i" + ann.getIntronNo();
+                                }
+
                                 String[] fields = ann.getHgvsCoding().split("del");
-                                nameTemp.add(ann.getFeature() + ":" + ann.getSymbol() + ":" + fields[0]);
+                                nameTemp.add(ann.getFeature() + ":" + ann.getSymbol() + ":" + exonOrIntronNumber + ":" + fields[0]);
                             }
 
+                        }
+
+                    }
+
+                    //print original name field
+                    if (!feature.getName().equals("")) {
+
+                        if (nameTemp.size() == 0){
+                            printWriter.write(feature.getName());
+                        } else {
+                            printWriter.write(feature.getName() + ";");
                         }
 
                     }
@@ -104,12 +130,12 @@ public class Main {
 
                 reader.close();
             } catch (IOException e){
-                log.log(Level.SEVERE, e.toString());
+                log.log(Level.SEVERE, "Could not read bed file: " + e.getMessage());
             }
 
             printWriter.close();
         } catch (IOException e){
-            log.log(Level.SEVERE, e.toString());
+            log.log(Level.SEVERE, "Could not write bed file: " + e.getMessage());
         }
 
     }
